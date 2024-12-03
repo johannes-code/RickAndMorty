0f4f717fb91cfa7ee880cd991d9f93eac2f465ea
scrapjo <darklord85@gmail.com>
2024-12-03 21:02:30 +0100

    fetch works for all level one items

--- script/script.js
+++ script/script.js
@@ -1,29 +1,83 @@
 fetch('https://rickandmortyapi.com/api/character/')
-  .then(res => res.json())
-  .then(json => {
-    const characters = json.results;
-    const container = document.getElementById('Characters-container');
+.then(res => res.json())
+.then(json => {
+  const characters = json.results;
+  const container = document.getElementById('Characters-container');
+  
+
+  characters.forEach(character => {
+      const characterDiv = document.createElement('div');
+      characterDiv.className = 'character';
+
+      characterDiv.innerHTML = `
+      <h2>${character.name}</h2>
+      <img src="${character.image}" alt="${character.name}">
+      <p>Character id: ${character.id}</p>
+      <p>Status: ${character.status}</p>
+      <p>Species: ${character.species}</p>
+      <p>Gender: ${character.gender}</p>
+      <p>Origin: ${character.origin.name}</p>
+      <p>Location: ${character.location.name}</p>
+    `;
+    container.appendChild(characterDiv);
+  });
+
+  })
+
+  .catch(error => console.error('Error', error));
     
 
-    characters.forEach(character => {
-        const characterDiv = document.createElement('div');
-        characterDiv.className = 'character';
+fetch('https://rickandmortyapi.com/api/location/')
+.then(res => res.json())
+.then(json => {
+  const location = json.results;
+  const container = document.getElementById('Location-container');
+  
 
-        characterDiv.innerHTML = `
-        <h2>${character.name}</h2>
-        <img src="${character.image}" alt="${character.name}">
-        <p>Status: ${character.status}</p>
-        <p>Species: ${character.species}</p>
-        <p>Gender: ${character.gender}</p>
-        <p>Origin: ${character.origin.name}</p>
-        <p>Location: ${character.location.name}</p>
-      `;
-      container.appendChild(characterDiv);
-    });
+  location.forEach(location => {
+      const locationDiv = document.createElement('div');
+      locationDiv.className = 'location';
 
-    })
+      locationDiv.innerHTML = `
+      <h2>${location.name}</h2>
+      <p>location id: ${location.id}</p>
+      <p>Type: ${location.type}</p>
+      <p>Dimension: ${location.dimension}</p>
+      <p>Residents: "PLACEHOLDER"</p>
+    `;
+    container.appendChild(locationDiv);
+  });
 
-    .catch(error => console.error('Error', error));
-    
+  })
+
+  .catch(error => console.error('Error', error));
+
+
+fetch('https://rickandmortyapi.com/api/episode/')
+.then(res => res.json())
+.then(json => {
+const episode = json.results;
+const container = document.getElementById('Episode-container');
+  if (!container) {
+    console.error('Episode container not found');
+    return;
+  }
+
+episode.forEach(episode => {
+    const episodeDiv = document.createElement('div');
+    episodeDiv.className = 'episode';
+
+    episodeDiv.innerHTML = `
+    <h2>${episode.name}</h2>
+    <p>Episode: ${episode.episode}</p>
+    <p>Air date: ${episode.air_date}</p>
+    <p>Characters: "PLACEHOLDER"</p>
+  `;
+  container.appendChild(episodeDiv);
+});
+
+})
+
+.catch(error => console.error('Error', error));
 
 
