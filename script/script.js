7f7b12ae708a7b51d367876cf91f884f162cbdb4
johannes-code <johannes24sfj@gmail.com>
2024-12-04 14:59:36 +0100

    added cards for location section, changed js code for fetch method

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-fetch('https://rickandmortyapi.com/api/character/')
-.then(res => res.json())
-.then(json => {
-  const characters = json.results;
+async function getCharacters() {
+  const response = await fetch('https://rickandmortyapi.com/api/character/');
+  const data = await response.json();
+  const characters = data.results;
   const container = document.getElementById('Characters-container');
   
 
@@ -9,14 +9,14 @@ fetch('https://rickandmortyapi.com/api/character/')
       const characterDiv = document.createElement('div');
       characterDiv.className = 'character';
 
-      const episodePromises = character.episode.map(url => fetch(url).then(res => res.json()));
+      const episodePromises = character.episode.slice(0, 5).map(url => fetch(url).then(res => res.json()));
       const episodes = await Promise.all(episodePromises);
       const episodesNames = episodes.map(episodes => episodes.name).join(' , ') 
       
-
       characterDiv.innerHTML = `
       <h3>${character.name}</h3>
       <img src="${character.image}" alt="${character.name}">
+      <div>
       <p><b>Character id:</b> ${character.id}</p>
       <p><b>Status:</b> ${character.status}</p>
       <p><b>Species:</b> ${character.species}</p>
@@ -24,43 +24,45 @@ fetch('https://rickandmortyapi.com/api/character/')
       <p><b>Origin:</b> ${character.origin.name}</p>
       <p><b>Location:</b> ${character.location.name}</p>
       <p><b>Episodes:</b> ${episodesNames}</p>
-    `;
+      </div>
+      `;
     container.appendChild(characterDiv);
   });
 
-  })    
-
-fetch('https://rickandmortyapi.com/api/location/')
-.then(res => res.json())
-.then(json => {
-  const location = json.results;
-  const container = document.getElementById('Location-container');
+  }
   
+async function getLocations() {
 
-  location.forEach(async (location) => {
-      const locationDiv = document.createElement('div');
-      locationDiv.className = 'location';
+const response = await fetch('https://rickandmortyapi.com/api/location/');
+const data = await response.json();
+const location = data.results;
+const container = document.getElementById('Location-container');
 
-      const residentsPromises = location.residents.slice(0, 5).map(url => fetch(url).then(res => res.json()));
-      const residents = await Promise.all(residentsPromises);
-      const residentsNames = residents.map(residents => residents.name).join(' , ');
+location.forEach(async (location) => {
+    const locationDiv = document.createElement('div');
+    locationDiv.className = 'location';
 
-      locationDiv.innerHTML = `
-      <h2>${location.name}</h2>
-      <p><b>location id:</b> ${location.id}</p>
-      <p><b>Type:</b> ${location.type}</p>
-      <p><b>Dimension:</b> ${location.dimension}</p>
-      <p><b>Residents:</b> ${residentsNames}</p>
-    `;
-    container.appendChild(locationDiv);
-  });
+    const residentsPromises = location.residents.slice(0, 5).map(url => fetch(url).then(res => res.json()));
+    const residents = await Promise.all(residentsPromises);
+    const residentsNames = residents.map(residents => residents.name).join(' , ');
 
-  })
+    locationDiv.innerHTML = `
+    <h3>${location.name}</h3>
+    <p><b>location id:</b> ${location.id}</p>
+    <p><b>Type:</b> ${location.type}</p>
+    <p><b>Dimension:</b> ${location.dimension}</p>
+    <p><b>Residents:</b> ${residentsNames}</p>
+  `;
+  container.appendChild(locationDiv);
+});
 
-fetch('https://rickandmortyapi.com/api/episode/')
-.then(res => res.json())
-.then(json => {
-const episode = json.results;
+}
+
+async function getEpisode() {
+
+const response = await fetch('https://rickandmortyapi.com/api/episode/');
+const data = await response.json();
+const episode = data.results;
 const container = document.getElementById('Episode-container');
 
 episode.forEach(async (episode) => {
@@ -72,7 +74,7 @@ episode.forEach(async (episode) => {
     const characterNames = characters.map(char => char.name).join(' , ');
 
     episodeDiv.innerHTML = `
-    <h2>${episode.name}</h2>
+    <h3>${episode.name}</h3>
     <p><b>Episode:</b> ${episode.episode}</p>
     <p><b>Air date:</b> ${episode.air_date}</p>
     <p><b>Characters:</b> ${characterNames}</p>
@@ -80,8 +82,9 @@ episode.forEach(async (episode) => {
   container.appendChild(episodeDiv);
 });
 
-})
-
-.catch(error => console.error('Error', error));
+}
 
 
+getCharacters()
+getLocations();
+getEpisode()
